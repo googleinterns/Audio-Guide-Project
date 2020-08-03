24072ec152f139639e3dd8439a4b133e11ad8ba7
bori00 <fazakasbori@gmail.com>
2020-08-03 09:18:29 +0000

    add tests for UserTest

--- src/test/java/com/google/sps/UserTest.java
+++ src/test/java/com/google/sps/UserTest.java
@@ -9,11 +9,35 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class UserTest {
-    private final String id = "userid";
+    private static final String ID = "userid";
+    private static final String SELF_INTRODUCTION = "I am the user";
+    private static final String IMG_URL = "/img.com";
 
     @Test
     public void getId(){
-        User newUser = new User.Builder(id).build();
-        Assert.assertEquals(newUser.getId(), id);
+        User newUser = new User.Builder(ID).build();
+        Assert.assertEquals(newUser.getId(), ID);
+    }
+
+    @Test
+    public void getSelfIntroduction(){
+        //user with preset selfIntroduction
+        User newUser = new User.Builder(ID).build();
+        Assert.assertEquals(newUser.getSelfIntroduction(), null);
+
+        //user with preset selfIntroduction
+        newUser = new User.Builder(ID).addSelfIntroduction(SELF_INTRODUCTION).build();
+        Assert.assertEquals(newUser.getSelfIntroduction(), SELF_INTRODUCTION);
+    }
+
+    @Test
+    public void getImgUrl(){
+        //user with preset imgUrl
+        User newUser = new User.Builder(ID).build();
+        Assert.assertEquals(newUser.getImgUrl(), null);
+
+        //user with preset selfIntroduction
+        newUser = new User.Builder(ID).addImgUrl(IMG_URL).build();
+        Assert.assertEquals(newUser.getImgUrl(), IMG_URL);
     }
 }
