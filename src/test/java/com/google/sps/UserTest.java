bc1a9bd4bb0e9422e2a150e111291714c13631d8
bori00 <fazakasbori@gmail.com>
2020-08-03 09:08:39 +0000

    import User package to UserTest

--- src/test/java/com/google/sps/UserTest.java
+++ src/test/java/com/google/sps/UserTest.java
@@ -1,5 +1,6 @@
 package com.google.sps;
 
+import com.google.sps.user.User;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +8,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public final class MeetingRequestTest {
- 
+public final class UserTest {
+    private final String id = "userid";
+
+    @Test
+    public void getId(){
+        User newUser = new User.Builder(id).build();
+        Assert.assertEquals(newUser.getId(), id);
+    }
 }
