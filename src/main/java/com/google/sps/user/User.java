07be7e94d2e97ad1ee42bbf303477fd079cf93e3
bori00 <fazakasbori@gmail.com>
2020-08-03 08:30:23 +0000

    create builder for user

--- src/main/java/com/google/sps/user/User.java
+++ src/main/java/com/google/sps/user/User.java
@@ -4,7 +4,36 @@ import org.jetbrains.annotations.Nullable;
 
 /** Stores the data related to one user */
 public class User {
-   private final String id;
-   @Nullable 
-   private String selfIntroduction, imgUrl;
+    private final String id;
+    @Nullable 
+    private final String selfIntroduction, imgUrl;
+
+    public static class Builder{
+        //Required
+        private final String id;
+        //Optional
+        private final String selfIntroduction, imgurl;
+
+        public Builder(String id){
+            this.id = id;
+        }
+
+        public Builder addImgUrl(String imgUrl){
+            this.imgUrl = imgUrl;
+        }
+
+        public Builder addSelfIntroduction(String selfIntroduction){
+            this.selfIntroduction = selfIntroduction;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+    }
+
+    private User(Builder builder){
+        this.id = builder.id;
+        this.selfIntroduction = builder.selfIntroduction;
+        this.imgUrl = builder.imgUrl;
+    }
 }
