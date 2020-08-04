0501e6aa2192aa58ce636dc89f6c7765b72d7ee0
bori00 <fazakasbori@gmail.com>
2020-08-04 12:04:30 +0000

    add doPost() for saving user's data

--- src/main/java/com/google/sps/servlets/UserServlet.java
+++ src/main/java/com/google/sps/servlets/UserServlet.java
@@ -30,19 +30,57 @@ import java.io.IOException;
 /** This servlet handles users' data */
 @WebServlet("/user-data")
 public class UserServlet extends HttpServlet {
+    public static final String NAME_INPUT = "name";
+    public static final String PUBLIC_PORTFOLIO_INPUT = "publicPortfolio";
+    public static final String PUBLIC_PORTFOLIO_INPUT_PUBLIC_VALUE = "public";
+    public static final String SELF_INTRODUCTION_INPUT = "selfIntroduction";
+    public static final String IMG_URL_INPUT = "imgUrl";
+
+     /** Saves the recently submitted userdata(updates it if the user already has some data saved). */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+        UserRepository myUserRepository = UserRepositoryFactory.getUserRepository(RepositoryType.DATASTORE);
+        User user = getUserFromRequest(request);
+        myUserRepository.saveUser(user);
+        response.sendRedirect("/portfolio.html");
+    }
 
     /** Returns the data of the user who is currently logged in. */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
         UserRepository myUserRepository = UserRepositoryFactory.getUserRepository(RepositoryType.DATASTORE);
-        User user = myUserRepository.getUser("userid");
+        User user = myUserRepository.getUser(getUserIdFromUserService());
         response.setContentType("application/json;");
         response.getWriter().println(convertToJsonUsingGson(user));
     }
 
-    public static String getUserIdFromUserService() {
+    private String getUserIdFromUserService() {
         UserService userService = UserServiceFactory.getUserService();
         return userService.getCurrentUser().getUserId();
+    }
+
+    private String getUserEmailFromUserService() {
+        UserService userService = UserServiceFactory.getUserService();
+        return userService.getCurrentUser().getEmail();
+    }
+
+    private User getUserFromRequest(HttpServletRequest request) {
+        String id = getUserIdFromUserService();
+        String email = getUserEmailFromUserService();
+        User.Builder newUserBuilder = new User.Builder(id, email);
+        String name = request.getParameter(NAME_INPUT);
+        if (!name.equals("")) {
+            newUserBuilder.setName(name);
+        }
+        String selfIntroduction = request.getParameter(SELF_INTRODUCTION_INPUT);
+        if (!selfIntroduction.equals("")) {
+            newUserBuilder.addSelfIntroduction(selfIntroduction);
+        }
+        String publicPortfolioStringValue = request.getParameter(PUBLIC_PORTFOLIO_INPUT);
+        if ( publicPortfolioStringValue.equals(PUBLIC_PORTFOLIO_INPUT_PUBLIC_VALUE)) {
+            newUserBuilder.setPublicPortfolio();
+        }
+        return newUserBuilder.build();
     }
 
     private String convertToJsonUsingGson(Object o) {
