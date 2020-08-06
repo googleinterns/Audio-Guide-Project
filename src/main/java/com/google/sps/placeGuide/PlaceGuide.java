f87d285c76e92cbc71954d8f466aab80b88046c1
Dennis Willie <denniswillie2000@gmail.com>
2020-08-06 15:21:30 +0000

    Fix indentations

--- src/main/java/com/google/sps/placeGuide/PlaceGuide.java
+++ src/main/java/com/google/sps/placeGuide/PlaceGuide.java
@@ -20,75 +20,75 @@ import java.util.List;
 /** Class containing place guide's information. */
 public class PlaceGuide {
 
-    private final String name, audioUrl, creatorId;
+  private final String name, audioUrl, creatorId;
 
-    // Acquired using Places API.
-    private final String placeId;
+  // Acquired using Places API.
+  private final String placeId;
 
-    private final PlaceCoordinate coord;
+  private final PlaceCoordinate coord;
 
-    // Specify how long user usually spends to follow this place guide in minutes.
-    @Nullable
-    private final int length;
+  // Specify how long user usually spends to follow this place guide in minutes.
+  @Nullable
+  private final int length;
 
-    @Nullable
-    private final String desc, imgUrl;
+  @Nullable
+  private final String desc, imgUrl;
 
-    // List of ids that belonged to users that saved a particular place guide.
-    @Nullable
-    private final List<String> saverId;
+  // List of ids that belonged to users that saved a particular place guide.
+  @Nullable
+  private final List<String> saverId;
 
-    private PlaceGuide(String name, String audioUrl, String creatorId, 
+  private PlaceGuide(String name, String audioUrl, String creatorId, 
                                     String placeId, PlaceCoordinate coord,
                                     List<String> saverId, int length, 
                                     String desc, String imgUrl) {
-        this.name = name;
-        this.audioUrl = audioUrl;
-        this.creatorId = creatorId;
-        this.placeId = placeId;
-        this.coord = coord;
-        this.saverId = saverId;
-        this.length = length;
-        this.desc = desc;
-        this.imgUrl = imgUrl;
-    }
+    this.name = name;
+    this.audioUrl = audioUrl;
+    this.creatorId = creatorId;
+    this.placeId = placeId;
+    this.coord = coord;
+    this.saverId = saverId;
+    this.length = length;
+    this.desc = desc;
+    this.imgUrl = imgUrl;
+  }
 
-    public static class Builder {
-        private final String name, audioUrl, creatorId, placeId;
-        private final PlaceCoordinate coord;
-        private List<String> saverId;
-        private int length;
-        private String desc, imgUrl;
+  public static class Builder {
+    private final String name, audioUrl, creatorId, placeId;
+    private final PlaceCoordinate coord;
+    private List<String> saverId;
+    private int length;
+    private String desc, imgUrl;
         
-        public Builder(String name, String audioUrl, String creatorId, 
+    public Builder(String name, String audioUrl, String creatorId, 
                                             String placeId, PlaceCoordinate coord) {
-            this.name = name;
-            this.audioUrl = audioUrl;
-            this.creatorId = creatorId;
-            this.placeId = placeId;
-            this.coord = coord;
-        }
-        public Builder setSaverId(List<String> saverId) {
-            this.saverId = saverId;
-            return this;
-        }
-        public Builder setLength(int length) {
-            this.length = length;
-            return this;
-        }
-        public Builder setDescription(String desc) {
-            this.desc = desc;
-            return this;
-        }
-        public Builder setImageUrl(String imgUrl) {
-            this.imgUrl = imgUrl;
-            return this;
-        }
-        public UserAuthenticationStatus build() {
-            return new PlaceGuide(name, audioUrl, creatorId, placeId, coord, saverId, length, 
-                                                                                desc, imgUrl);
-        }
+      this.name = name;
+      this.audioUrl = audioUrl;
+      this.creatorId = creatorId;
+      this.placeId = placeId;
+      this.coord = coord;
     }
+    public Builder setSaverId(List<String> saverId) {
+      this.saverId = saverId;
+      return this;
+    }
+    public Builder setLength(int length) {
+      this.length = length;
+      return this;
+    }
+    public Builder setDescription(String desc) {
+      this.desc = desc;
+      return this;
+    }
+    public Builder setImageUrl(String imgUrl) {
+      this.imgUrl = imgUrl;
+      return this;
+    }
+    public UserAuthenticationStatus build() {
+      return new PlaceGuide(name, audioUrl, creatorId, placeId, coord, saverId, length, 
+                                                                            desc, imgUrl);
+    }
+  }
 
     public String getName() {
         return name;
