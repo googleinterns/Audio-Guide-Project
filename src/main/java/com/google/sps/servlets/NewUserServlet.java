db537c913904c55e0114096f250da2355ab260a9
bori00 <fazakasbori@gmail.com>
2020-08-06 20:29:47 +0000

    create NewUserServlet

---
+++ src/main/java/com/google/sps/servlets/NewUserServlet.java
@@ -0,0 +1,84 @@
+// Copyright 2020 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.RepositoryType;
+import com.google.sps.user.User;
+import com.google.sps.user.repository.UserRepository;
+import com.google.sps.user.repository.UserRepositoryFactory;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import java.io.IOException;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This servlet checks if the currently logged in user exists in the database
+ * and saves a new user in the database. 
+ */
+@WebServlet("/new-user-servlet")
+public class NewUserServlet extends HttpServlet {
+  private final UserRepository userRepository;
+  private final UserService userService;
+
+  public NewUserServlet() {
+    userRepository = UserRepositoryFactory.getUserRepository(RepositoryType.DATASTORE);
+    userService = UserServiceFactory.getUserService();
+  }
+
+  /**
+   * Saves the new user's data(id and email only, provided by the UserService) to the database.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    User user = getLoggedInUser();
+    userRepository.saveUser(user);
+    response.sendRedirect("/index.html");
+  }
+
+  /**
+   * @return true if the currently logged in user is already present in the database, false otherwise.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    boolean existingUser = userRepository.existingUser(userService.getCurrentUser().getUserId());
+    response.setContentType("application/json;");
+    response.getWriter().println(convertToJsonUsingGson(Boolean.valueOf(existingUser)));
+  }
+
+  private User getLoggedInUser(HttpServletRequest request) {
+    String id = userService.getCurrentUser().getUserId();
+    String email = userService.getCurrentUser().getEmail();
+    User.Builder newUserBuilder = new User.Builder(id, email);
+    return newUserBuilder.build();
+  }
+
+  private String convertToJsonUsingGson(Object o) {
+    Gson gson = new Gson();
+    String json = gson.toJson(o);
+    return json;
+  }
+}
