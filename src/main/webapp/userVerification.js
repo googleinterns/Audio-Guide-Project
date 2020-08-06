dbae2b49e00295a336b2b9c6f31fc390fa458303
Dennis Willie <denniswillie2000@gmail.com>
2020-08-06 11:33:25 +0000

    fix indentations

--- src/main/webapp/userVerification.js
+++ src/main/webapp/userVerification.js
@@ -9,15 +9,15 @@
 * in.
 */
 function authenticateUser() {
-    const currentWindowLocation = window.location.href;
-    const currentUrl = new URL(currentWindowLocation);
-    queryAuthenticationServer(currentUrl).then((userAuthenticationStatus) => {
-        if (!userAuthenticationStatus.isLoggedIn) {
-            location.replace(userAuthenticationStatus.loginUrl);
-        } else {
-            addLinktoLogoutButton(userAuthenticationStatus.logoutUrl);
-        }
-    });
+  const currentWindowLocation = window.location.href;
+  const currentUrl = new URL(currentWindowLocation);
+  queryAuthenticationServer(currentUrl).then((userAuthenticationStatus) => {
+    if (!userAuthenticationStatus.isLoggedIn) {
+      location.replace(userAuthenticationStatus.loginUrl);
+    } else {
+      addLinktoLogoutButton(userAuthenticationStatus.logoutUrl);
+    }
+  });
 }
 
 /**
@@ -25,15 +25,15 @@ function authenticateUser() {
 * as the query parameters.
 */
 function  queryAuthenticationServer(currentUrl) {
-    const requestUrl = new URL('/user-authentication', currentUrl);
-    const queryParams = new QueryParams(currentUrl);
-    requestUrl.search = new URLSearchParams(queryParams).toString();
+  const requestUrl = new URL('/user-authentication', currentUrl);
+  const queryParams = new QueryParams(currentUrl);
+  requestUrl.search = new URLSearchParams(queryParams).toString();
 
-    return fetch(requestUrl, {method: 'GET'})
-        .catch(error => console.log(error))
-        .then((response) => {
-            return response.json();
-        });
+  return fetch(requestUrl, {method: 'GET'})
+    .catch(error => console.log(error))
+    .then((response) => {
+      return response.json();
+    });
 }
 
 class QueryParams{
@@ -43,8 +43,8 @@ class QueryParams{
 }
 
 function addLinktoLogoutButton(logoutUrl) {
-    const logoutButton = document.getElementById('logout');
-    logoutButton.addEventListener('click', () => {
-    window.location.replace(logoutUrl);
-    });
+  const logoutButton = document.getElementById('logout');
+  logoutButton.addEventListener('click', () => {
+  window.location.replace(logoutUrl);
+  });
 }
