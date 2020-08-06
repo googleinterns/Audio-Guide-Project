b461549c901a056d893fc0acb7e7ff1ad8cdf742
bori00 <fazakasbori@gmail.com>
2020-08-06 16:49:38 +0000

    make connection between front and backend

--- src/main/webapp/portfolioForm.js
+++ src/main/webapp/portfolioForm.js
@@ -1,4 +1,18 @@
-function addBlobstoreUploadurlToForm() {
+// Copyright 2020 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+function addBlobstoreUploadUrlToForm() {
   fetch('/blobstore-upload-portfolio')
       .catch(error => console.log("blobstore-upload-portfolio: failed to fetch: " + error))
       .then(uploadUrl => uploadUrl.text())
@@ -9,7 +23,8 @@ function addBlobstoreUploadurlToForm() {
       });
 }
 
+/** Set the destination url of the post method for the portfolio form t */
 function setFormActionUrl(uploadUrl){
-  var form = Document().getElementById("portfolioForm");
+  var form = document.getElementById("portfolioForm");
   form.action = uploadUrl;
 }
