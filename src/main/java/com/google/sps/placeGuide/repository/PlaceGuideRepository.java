e2ed713881c7b3d7b2bab270a1d3ff7e8c4d0f67
Dennis Willie <denniswillie2000@gmail.com>
2020-08-06 01:46:03 +0000

    add new method

--- src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
+++ src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public interface PlaceGuideRepository {
 
-    public void createAndSavePlaceGuide(PlaceGuide placeGuide);
+    public void createAndStorePlaceGuide(PlaceGuide placeGuide);
 
     // For when user wants to save the user's own place guide or other user's place guide.
     public void savePlaceGuide(String creatorId, String placeId);
@@ -37,7 +37,8 @@ public interface PlaceGuideRepository {
 
     // For marking all the place guides that the user created on the current map window.
     @Nullable
-    public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId, List<PlaceCoordinate> coordinatesList);
+    public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId, 
+                                                    List<PlaceCoordinate> coordinatesList);
 
     // For when trying to get saved place guides for the info windows.
     @Nullable
@@ -45,8 +46,11 @@ public interface PlaceGuideRepository {
 
     // For marking all the place guides that the user saved on the current map window.
     @Nullable
-    public List<PlaceGuide> getSavedPlaceGuidesList(String saverId, List<PlaceCoordinate> coordinatesList);
+    public List<PlaceGuide> getSavedPlaceGuidesList(String saverId, 
+                                                    List<PlaceCoordinate> coordinatesList);
 
-    public void deleteSelectedPlaceGuide(String creatorId, String placeId);
+    public void deleteSelectedCreatedPlaceGuide(String creatorId, String placeId);
+
+    public void removeSelectedSavedPlaceGuide(String saverId, String placeId);
     
 }
