c0996d3744bd1ef50e09ac288cba04877e78fd20
Dennis Willie <denniswillie2000@gmail.com>
2020-08-07 00:56:17 +0000

    Add changes

--- src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
+++ src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
@@ -1,4 +1,4 @@
-package com.google.sps.repository;
+package com.google.sps.placeGuide.repository;
 
 import org.jetbrains.annotations.Nullable;
 import com.google.sps.placeGuide.PlaceGuide;
@@ -7,36 +7,36 @@ import java.util.List;
 
 public interface PlaceGuideRepository {
 
-    public void createAndStorePlaceGuide(PlaceGuide placeGuide);
+  public void createAndStorePlaceGuide(PlaceGuide placeGuide);
 
-    // For when user wants to save the user's own place guide or other user's place guide.
-    public void savePlaceGuide(String creatorId, String placeId);
+  // For when user wants to save the user's own place guide or other user's place guide.
+  public void savePlaceGuide(String creatorId, String placeId);
 
-    // For when navigating from the map and get all the place guides data in the
-    // current window view.
-    @Nullable
-    public List<PlaceGuide> getPlaceGuidesList(List<PlaceCoordinate>);
+  // For when navigating from the map and get all the place guides data in the
+  // current window view.
+  @Nullable
+  public List<PlaceGuide> getPlaceGuidesList(List<PlaceCoordinate>);
 
-    // For when trying to get personal created place guides for the info windows.
-    @Nullable
-    public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId);
+  // For when trying to get personal created place guides for the info windows.
+  @Nullable
+  public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId);
 
-    // For marking all the place guides that the user created on the current map window.
-    @Nullable
-    public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId, 
+  // For marking all the place guides that the user created on the current map window.
+  @Nullable
+  public List<PlaceGuide> getCreatedPlaceGuidesList(String creatorId, 
                                                     List<PlaceCoordinate> coordinatesList);
 
-    // For when trying to get saved place guides for the info windows.
-    @Nullable
-    public List<PlaceGuide> getSavedPlaceGuidesList(String saverId);
+  // For when trying to get saved place guides for the info windows.
+  @Nullable
+  public List<PlaceGuide> getSavedPlaceGuidesList(String saverId);
 
-    // For marking all the place guides that the user saved on the current map window.
-    @Nullable
-    public List<PlaceGuide> getSavedPlaceGuidesList(String saverId, 
+  // For marking all the place guides that the user saved on the current map window.
+  @Nullable
+  public List<PlaceGuide> getSavedPlaceGuidesList(String saverId, 
                                                     List<PlaceCoordinate> coordinatesList);
 
-    public void deleteSelectedCreatedPlaceGuide(String creatorId, String placeId);
+  public void deleteSelectedCreatedPlaceGuide(String creatorId, String placeId);
 
-    public void removeSelectedSavedPlaceGuide(String saverId, String placeId);
+  public void removeSelectedSavedPlaceGuide(String saverId, String placeId);
     
 }
---
+++ src/main/java/com/google/sps/placeGuide/repository/impl/DatastorePlaceGuideRepository.java
@@ -0,0 +1,7 @@
+package com.google.sps.placeGuide.repository.impl;
+
+import com.google.sps.placeGuide.repository.PlaceGuideRepository;
+
+public class DatastorePlaceGuideRepository implements PlaceGuideRepository{
+  
+}
