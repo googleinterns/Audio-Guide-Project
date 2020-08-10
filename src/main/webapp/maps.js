973470ff4a92ed0c167a46eceebb7b0031b0f396
bori00 <fazakasbori@gmail.com>
2020-08-10 08:14:13 +0000

    center map at user's location if permission is given

--- src/main/webapp/maps.js
+++ src/main/webapp/maps.js
@@ -7,4 +7,22 @@ function createMap() {
     };
     const map = new google.maps.Map(
         document.getElementById('map'), myMapOptions); 
+    centerMapAtUsersLocation(map);
+}
+
+function centerMapAtUsersLocation(map) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+            map.setZoom(7);
+        }, function() {
+            alert("The geolocation service failed.");
+        });
+    } else {
+        alert("The browser doesn't support geolocation.");
+    }
 }
