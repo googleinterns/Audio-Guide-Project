ff1a80a3a5bd390c3759dfb1a7341463c8e13839
bori00 <fazakasbori@gmail.com>
2020-08-10 19:53:14 +0000

    add support for pressing enter in searchbox wthout choosing a suggested place

--- src/main/webapp/maps.js
+++ src/main/webapp/maps.js
@@ -27,27 +27,58 @@ function initAutocomplete(map) {
     searchedPlaceMarker.setMap(map);
 
     autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+    autocomplete.addListener('place_changed', result => showNewSearchResult(map, autocomplete, input, searchedPlaceMarker));
+}
 
-    autocomplete.addListener('place_changed', function() {
-        searchedPlaceMarker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("Place not found: '" + place.name + "'");
+function showNewSearchResult(map, autocomplete, input, searchedPlaceMarker) {
+    searchedPlaceMarker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        // If the user didn't choose a suggestion, but pressed enter, 
+        // then "place" may have no geometry. 
+        // In this case, search for the "closest" place with geometry and show that.
+        showBestSearchResultOnEnter(map, input, autocomplete, searchedPlaceMarker);
+        return;
+    }
+    displaySearchResultOnMap(map, place, searchedPlaceMarker, true);
+}
+
+function displaySearchResultOnMap(map, place, placeMarker, centerMap) {
+    if (centerMap) {
+        centerMapAroundPlace(map, place);
+    }
+    placeMarker.setPosition(place.geometry.location);
+    placeMarker.setVisible(true);
+}
+
+function centerMapAroundPlace(map, place) {
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(placeZoom);
+    }
+}
+
+function showBestSearchResultOnEnter(map, input, autocomplete, searchedPlaceMarker) {
+    var service = new google.maps.places.AutocompleteService();
+    service.getPlacePredictions({ input:  input.value}, function(predictions, status){
+        if (status != google.maps.places.PlacesServiceStatus.OK) {
+            alert(status);
             return;
         }
+        showPlaceBasedOnId(map, predictions[0].place_id, searchedPlaceMarker);
+    });
+}
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(placeZoom);
-        }
-
-        searchedPlaceMarker.setPosition(place.geometry.location);
-        searchedPlaceMarker.setVisible(true);
+function showPlaceBasedOnId(map, id, placeMarker) {
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({placeId: id}, (results, status) => {
+      if (status !== "OK") {
+        window.alert("Geocoder failed due to: " + status);
+        return;
+      }
+      displaySearchResultOnMap(map, results[0], placeMarker, true);
     });
 }
 
@@ -71,7 +102,7 @@ function addGoToMyLocationControl(map) {
                 alert("The browser doesn't support geolocation.");
             }
         } else {
-             console.log("stop watchPosition");
+            console.log("stop watchPosition");
             navigator.geolocation.clearWatch(watchPositionId);
             removeCurrentLocationMarker();
             img.src = "./img/my_location.svg";
