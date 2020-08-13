3ce2f1205d356557f2d110d3efa0e91fb0ec8a90
bori00 <fazakasbori@gmail.com>
2020-08-13 12:43:03 +0000

    create createPlaeGuide page

--- src/main/webapp/maps_show_places.js
+++ src/main/webapp/maps_show_places.js
@@ -8,6 +8,16 @@ function displayPublicPlaceGuides(map) {
              {imagePath: './img/m'});
 }
 
+function displayPlaceGuidesOfUser(map, userId) {
+    var placeGuides = getPlaceGuidesOfUser(userId);
+    placeGuides.forEach(placeGuide => placeGuide.map = map);
+    var markers = placeGuides.map(function(placeGuide) { 
+        return placeGuide._marker;
+    });
+    var markerCluster = new MarkerClusterer(map, markers,
+             {imagePath: './img/m'});
+}
+
 // This is for demonstrational purposes only. data will be fetched from the server.
 function getPublicPlaceGuides() {
     var publicPlaceGuides = [
@@ -29,6 +39,27 @@ function getPublicPlaceGuides() {
             51.834982,-86.723060, "placeId7", "creatorId8", "creatorName8",  PlaceType.PUBLIC),
         new PlaceGuide("id9", "PlaceGuide 9", "This is a placeguide 9", "audioKey9", 6, "imgKey9", 
             46.770003,23.509997, "placeId8", "creatorId9", "creatorName9",  PlaceType.PUBLIC),
+    ];
+    return publicPlaceGuides;
+}
+
+// This is for demonstrational purposes only. data will be fetched from the server.
+function getPlaceGuidesOfUser(userId) {
+    var publicPlaceGuides = [
+        new PlaceGuide("id1", "PlaceGuide 1", "This is a placeguide 1", "audioKey1", 3, "imgKey1", 
+            46.731686,23.604655, "placeId1", userId, "creatorName1",  PlaceType.PUBLIC),
+        new PlaceGuide("id2", "PlaceGuide 2", "This is a placeguide 2", "audioKey2", 4, "imgKey2", 
+            46.837006,23.527699, "placeId2", userId, "creatorName2",  PlaceType.PUBLIC),
+        new PlaceGuide("id3", "PlaceGuide 3", "This is a placeguide 3", "audioKey3", 2, "imgKey3", 
+            46.162006,25.469789, "placeId3", userId, "creatorName3",  PlaceType.PRIVATE),
+        new PlaceGuide("id4", "PlaceGuide 4", "This is a placeguide 4", "audioKey4", 12, "imgKey4", 
+            46.295773,2.465071, "placeId4", userId, "creatorName4",  PlaceType.PRIVATE),
+        new PlaceGuide("id5", "PlaceGuide 5", "This is a placeguide 5", "audioKey5", 8, "imgKey5", 
+            39.751611,-3.163685, "placeId4", userId, "creatorName5",  PlaceType.PRIVATE),
+        new PlaceGuide("id8", "PlaceGuide 8", "This is a placeguide 8", "audioKey8", 4, "imgKey8", 
+            51.834982,-86.723060, "placeId7", userId, "creatorName8",  PlaceType.PUBLIC),
+        new PlaceGuide("id9", "PlaceGuide 9", "This is a placeguide 9", "audioKey9", 6, "imgKey9", 
+            46.770003,23.509997, "placeId8", userId, "creatorName9",  PlaceType.PRIVATE),
     ];
     return publicPlaceGuides;
 }
