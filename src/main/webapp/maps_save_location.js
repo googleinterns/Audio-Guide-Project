c05499eb21eed56e24c45a2c296456f4b26c6028
bori00 <fazakasbori@gmail.com>
2020-08-13 19:29:11 +0000

    add saveLocationControl to createPlaceGuide page

--- src/main/webapp/maps_save_location.js
+++ src/main/webapp/maps_save_location.js
@@ -2,16 +2,22 @@ class LocationSaver {
     static SAVE_LOCATION_TEXT = "Save location";
     static SAVE_LOCATION_ID = "locationSaverControl";
 
-    constructor(chosenPlace) {
+    constructor(map, chosenPlace) {
+        this._map = map;
         this._savedPlace = null;
         this._chosenPlace = chosenPlace; 
         this._saveLocationControlDiv = this.createControlDiv(LocationSaver.SAVE_LOCATION_TEXT, LocationSaver.SAVE_LOCATION_ID);
+        this._map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(this._saveLocationControlDiv);
+    }
+
+    init() {
+
     }
 
     /** Creates a div with the style of control elements and with the given text. */
     createControlDiv(text, id) {
         const controlDiv = document.createElement("div");
-        controlDiv.setAttribute('class', 'control');
+        controlDiv.setAttribute('class', 'map-button');
         controlDiv.setAttribute('id', id);
         controlDiv.innerText = text;
         return controlDiv;
