b6ae191f5305a9d3b99a602a44561966870fc744
bori00 <fazakasbori@gmail.com>
2020-08-17 16:11:22 +0000

    remove scripts from html files

---
+++ src/main/webapp/maps/discover_createMap.js
@@ -0,0 +1,33 @@
+/** 
+        * This function initialises the map and adds some functionalities to it:
+        * Geolocation: to track the user's current location, display it and center the map around it.
+        * Searching: lets the user search for places and center the map around it.
+        * Display place guides: displays all the public place guides.
+        */
+        function createMap() {
+            var myMapOptions = {
+                zoom: 2,
+                center: new google.maps.LatLng(0, 0),
+                mapTypeId: 'roadmap',
+                restriction: {
+                  latLngBounds: {north: 85, south: -85, west: -180, east: 180},
+                  strictBounds: true
+                }
+            };
+            const map = new google.maps.Map(
+                document.getElementById('map'), myMapOptions);
+
+            // Geolocation functionality.
+            var geolocator = new Geolocator(map);
+            geolocator.init();
+
+            // Searching functionality.
+            var searchResult =
+                Place.constructPlaceBasedOnCoordinates(map, 0, 0, "Search Result", PlaceType.SEARCH_RESULT, true);
+            searchResult.visible = false;
+            var searchBox = new SearchBox(map, searchResult, "search-box");
+            searchBox.init();
+
+            // Discover public place guides functionality.
+            PlaceDisplayer.displayPublicPlaceGuides(map);
+        }
