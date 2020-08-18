327baa2851ff293cece0e8b4a08d1f2a95eeda49
bori00 <fazakasbori@gmail.com>
2020-08-18 08:13:58 +0000

    add arguments to query functions

--- src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
+++ src/main/java/com/google/sps/placeGuide/repository/PlaceGuideRepository.java
@@ -2,6 +2,7 @@ package com.google.sps.placeGuide.repository;
 
 import org.jetbrains.annotations.Nullable;
 import com.google.sps.placeGuide.PlaceGuide;
+import com.google.appengine.api.datastore.GeoPt;
 import java.util.List;
 
 /** Interface for handling place guide. */
@@ -26,18 +27,18 @@ public interface PlaceGuideRepository {
   public void deletePlaceGuide(long placeGuideId);
 
   // @return all public {@code PlaceGuide} which are within a given map area of rectangular form.
-  public List<PlaceGuide> getAllPublicPlaceGuidesInMapArea();
+  public List<PlaceGuide> getAllPublicPlaceGuidesInMapArea(GeoPt northEastCorner, GeoPt southWestCorner);
 
   // Finds all of the user's created place guides which are within a given map area of rectangular form.
-  public List<PlaceGuide> getCreatedPlaceGuides(String creatorId);
+  public List<PlaceGuide> getCreatedPlaceGuidesInMapArea(String creatorId, GeoPt northEastCorner, GeoPt southWestCorner);
 
   // Finds all of the user's created place guides that are public and 
   // which are within a given map area of rectangular form.
   // (available to be viewed by other users).
-  public List<PlaceGuide> getCreatedPublicPlaceGuides(String creatorId);
+  public List<PlaceGuide> getCreatedPublicPlaceGuidesInMapArea(String creatorId, GeoPt northEastCorner, GeoPt southWestCorner);
 
   // Finds all of the user's created place guides that are private and
   // which are within a given map area of rectangular form.
   // (not available to be viewed by other users).
-  public List<PlaceGuide> getCreatedPrivatePlaceGuides(String creatorId);
+  public List<PlaceGuide> getCreatedPrivatePlaceGuidesInMapArea(String creatorId, GeoPt northEastCorner, GeoPt southWestCorner);
 }
