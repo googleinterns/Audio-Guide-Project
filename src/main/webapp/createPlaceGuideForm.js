e0e7e61ecb278a18242ca26c955cbbd168a59864
Dennis Willie <denniswillie2000@gmail.com>
2020-08-19 22:21:55 +0000

    remove duplicating code, add a new script file

--- src/main/webapp/createPlaceGuideForm.js
+++ src/main/webapp/createPlaceGuideForm.js
@@ -41,39 +41,6 @@ function activatePreviewFeature() {
   });
 }
 
-/**
- * Sets the action of the form to the url to which blobs can be uploaded.
- */
-function addBlobstoreUploadUrlToForm(formType, formId) {
-  getBlobstoreUploadUrlFromServlet(formType).then(uploadUrl => {
-    setFormActionUrl(uploadUrl, formId);
-  });
-}
-
-/**
- * Fetches a url to which the files can be uploaded, relying on Blobstore API.
- */
-function getBlobstoreUploadUrlFromServlet(formType) {
-  var url = new URL("/blobstore-upload-url", document.URL);
-  url.searchParams.append('formType', formType)
-  return fetch(url)
-      .catch(error => console.log("getBlobstoreUploadUrlFromServlet: failed to fetch: " + error))
-      .then(uploadUrl => uploadUrl.text())
-      .catch(error =>
-          console.log('getBlobstoreUploadUrlFromServlet: failed to convert to text: ' + error))
-      .then(uploadUrl => {
-        return uploadUrl;
-      });
-}
-
-/**
- * Sets the destination url of the post method for the createPlaceGuide form to uploadUrl.
- */
-function setFormActionUrl(uploadUrl, formId) {
-  var form = document.getElementById("formId");
-  form.action = uploadUrl;
-}
-
 // Just a test by fetching actual place guides' data from database 
 // to see if image and audio previewing also works with files from blobstore.
 function testExistingPlaceGuide() {
@@ -87,17 +54,6 @@ async function getFetchedList() {
   .then((response) => {
       return response.json();
   });
-}
-
-/**
- * Sets one form input's value to the given value.
- */
-function setFormInputValue(input, value) {
-  if (value === undefined) {
-    input.value = "";
-  } else {
-    input.value = value;
-  }
 }
 
 function testGivenCoordinate() {
