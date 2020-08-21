438167bbf64803a61a9cd44b8a66b616798cff4e
bori00 <fazakasbori@gmail.com>
2020-08-21 10:48:12 +0000

    make placeGuideManager and map global

--- src/main/webapp/maps/discover_createMap.js
+++ src/main/webapp/maps/discover_createMap.js
@@ -4,10 +4,13 @@
  * Searching: lets the user search for places and center the map around it.
  * Display place guides: displays all the public place guides.
  */
+var map;
+var placeGuideManager;
+
 function createMap() {
    var map = new MapWidget();
    map.addGeolocationFunctionality();
    map.addSearchingFunctionality();
    var placeGuideRepository = new PlaceGuideRepository(PlaceGuideRepository.QueryType.ALL_PUBLIC_IN_MAP_AREA);
-   var placeGuideManager = new PlaceGuideManager(map, placeGuideRepository);
+   placeGuideManager = new PlaceGuideManager(map, placeGuideRepository);
 }
