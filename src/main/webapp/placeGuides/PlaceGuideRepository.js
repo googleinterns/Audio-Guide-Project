3814c835b513807faa2c3c60a53137dd84a8798e
bori00 <fazakasbori@gmail.com>
2020-08-21 06:38:24 +0000

    remove placeGuide in repository

--- src/main/webapp/placeGuides/PlaceGuideRepository.js
+++ src/main/webapp/placeGuides/PlaceGuideRepository.js
@@ -4,7 +4,6 @@ class PlaceGuideRepository {
         CREATED_ALL_IN_MAP_AREA: "CREATED_ALL_IN_MAP_AREA",
         CREATED_PUBLIC_IN_MAP_AREA: "CREATED_PUBLIC_IN_MAP_AREA",
         CREATED_PRIVATE_IN_MAP_AREA: "CREATED_PRIVATE_IN_MAP_AREA",
-        BOOKMARKED: "BOOKMARKED_IN_MAP_AREA",
     };
     
     constructor(queryType) {
@@ -12,4 +11,21 @@ class PlaceGuideRepository {
         this._placeGuides = {}; // Init dictionary. 
     }
 
+    updatePlaceGuides() {
+
+    }
+
+    get placeGuides() {
+        return this._placeGuides;
+    }
+
+    removePlaceGuide(placeGuideId) {
+        // Remove from in-memory dictionary.
+        delete this._placeGuides[placeGuideId];
+        // Remove from database.
+        var url = new URL("/delete-place-guide-data", document.URL);
+        url.searchParams.append('id', formType)
+        return fetch(url)
+            .catch(error => console.log("DeletePlaceGuideServlet: failed to fetch: " + error));
+    }
 }
