bfde817318d99d2e96c0cd4a0616ada4630391d1
bori00 <fazakasbori@gmail.com>
2020-08-21 10:19:06 +0000

    implement update for MapPlaceGuideDisplayer

--- src/main/webapp/placeGuides/MapPlaceGuideDisplayer.js
+++ src/main/webapp/placeGuides/MapPlaceGuideDisplayer.js
@@ -6,7 +6,47 @@ class MapPlaceGuideDisplayer {
     }
 
     update(placeGuides) {
+        this.removePreviousPlaceGuidesFromMap(placeGuides);
+        this.addNewPlaceGuidesToMap(placeGuides);
+    }
 
+    removePreviousPlaceGuidesFromMap(placeGuides) {
+        for (var placeGuideId in this._placeGuidesOnMap) {
+            if (this._placeGuidesOnMap.hasOwnProperty(placeGuideId)) {           
+                if(!placeGuides.hasOwnProperty(placeGuideId)) {
+                    // This placeGuide is not needed anymore.
+                    this._placeGuidesOnMap[placeGuideId].remove();
+                }
+            }
+        }
+    }
+
+    addNewPlaceGuidesToMap(placeGuides) {
+        for (var placeGuideId in placeGuides) {
+            if (placeGuides.hasOwnProperty(placeGuideId)) {           
+                if(!this._placeGuidesOnMap.hasOwnProperty(placeGuideId)) {
+                    // new placeGuide should be constructed.
+                    this._placeGuidesOnMap[placeGuideId].remove();
+                }
+            }
+        }
+    }
+
+    constructPlaceGuideOnMapFromPlaceGuide(placeGuide) {
+        var placeType;
+        if (placeGuide.isPublic) {
+            placeType = placeType.PUBLIC;
+        } else {
+            placeType = placeType.PRIVATE;
+        }
+        return new PlaceGuideOnMap(this._map, 
+                                   placeGuide.id, 
+                                   placeGuide.name, 
+                                   placeGuide.position, 
+                                   placeGuide.mapsPlace, 
+                                   placeGuide.creator,
+                                   placeGuide.description, 
+                                   placeType);
     }
 
     remove(placeGuideId) {
