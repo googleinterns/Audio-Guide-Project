0ed5d5f46f4c86bfcee3dcfd2f01892fdd93265b
bori00 <fazakasbori@gmail.com>
2020-08-21 06:07:53 +0000

    create user class

---
+++ src/main/webapp/user/userClass.js
@@ -0,0 +1,33 @@
+class User {
+    constructor(id, email, name, publicPortfolio, selfIntroduction, imgKey) {
+        this._id = id;
+        this._email = email;
+        this._name = name;
+        this._publicPortfolio = publicPortfolio;
+        this.imgKey = imgKey;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get publicPortfolio() {
+        return this._publicPortfolio;
+    }
+
+    get selfIntroduction() {
+        return this._selfIntroduction;
+    }
+
+    get imgKey() {
+        return this._imgKey;
+    }
+}
