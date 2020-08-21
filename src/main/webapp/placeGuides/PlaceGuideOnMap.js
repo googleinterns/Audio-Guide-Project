adb1126bafe534da3af0542cfbf67d169c5825ec
bori00 <fazakasbori@gmail.com>
2020-08-21 09:23:21 +0000

    set up marker and infowindow for PlaceGuideOnMap

--- src/main/webapp/placeGuides/PlaceGuideOnMap.js
+++ src/main/webapp/placeGuides/PlaceGuideOnMap.js
@@ -1,9 +1,11 @@
 class PlaceGuideOnMap {
     constructor(map, id, name, position, place, creator, description, placeType) {
         this.marker = setupMarker(map, placeType, name, position);
+        this._infoWindowClosed = true;
+        this._infoWindow = getInfoWindow(name, position, place, creator, description);
     }
 
-    static setupMarker(map, placeType, name, position) {
+    static getMarker(map, placeType, name, position) {
         var markerIcon;
         if (placeType.icon != null) {
             markerIcon = this._placeType.icon;
@@ -17,6 +19,27 @@ class PlaceGuideOnMap {
             map: map,
         });
         return marker;
+    }
+
+    static getInfoWindow(name, position, place, creator, description) {
+        return new google.maps.InfoWindow({
+            content: this.getInfoWindowContent(name, position, place, creator, description),
+            maxWidth: 200,
+        });
+    }
+
+    static getInfoWindowContent(name, position, place, creator, description) {
+        var placeName;
+        if (place != null) {
+            placeName = place.name;
+        } else {
+            placeName = position.toString();
+        }
+        var content = "<h3>" + name + "</h3>" +
+            "<h4> Created by: " + creator.name + "</h4>" +
+            "<h4> Place: " + placeName + "</h4>" +
+            "<p>" + description + "</p>";
+        return content;
     }
 
     remove() {
