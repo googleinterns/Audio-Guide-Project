8a0d0ab14e24fa2054551a8a5932dd6f9a5e7c21
bori00 <fazakasbori@gmail.com>
2020-08-24 19:17:13 +0000

    add nre PlaceGudieManaer and MapWidget to all pages

--- src/main/webapp/maps/createPlaceGuide_initPage.js
+++ src/main/webapp/maps/createPlaceGuide_initPage.js
@@ -8,9 +8,18 @@
  * creation.
  * Display place guides: displays the place guides created by the currently logged in user.
  */
+
+var map;
+var placeGuideManager;
+
 function initPage() {
-  var map = new MapWidget();
-   map.addGeolocationFunctionality();
-   map.addLocationChoosingAndSavingFunctionality();
-  //PlaceDisplayer.displayPlaceGuidesOfUser(map);
+   var mapWidget = new MapWidget();
+   mapWidget.addGeolocationFunctionality();
+   mapWidget.addLocationChoosingAndSavingFunctionality();
+   map = mapWidget.map;
+   var placeGuideRepository = new PlaceGuideRepository(PlaceGuideRepository.QueryType.CREATED_ALL_IN_MAP_AREA);
+   placeGuideManager = new PlaceGuideManager(map, placeGuideRepository);
+   var corner1 = new google.maps.LatLng(10, 10);
+   var corner2 = new google.maps.LatLng(10, 10);
+   placeGuideManager.update(new google.maps.LatLngBounds(corner1, corner2), 12);
 }
