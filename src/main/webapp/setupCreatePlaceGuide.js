cd8ea6c14e1a2c01a25f259c987af696876d4c3a
Dennis Willie <denniswillie2000@gmail.com>
2020-08-24 10:13:50 +0000

    delete placeGuideToEdit

--- src/main/webapp/setupCreatePlaceGuide.js
+++ src/main/webapp/setupCreatePlaceGuide.js
@@ -1,10 +1,10 @@
-function setUpCreatePlaceGuidePage(placeGuideToEdit) {
+function setUpCreatePlaceGuidePage() {
   authenticateUser().then(userAuthenticationStatus => {
     if (!userAuthenticationStatus.isLoggedIn) {
       location.replace(userAuthenticationStatus.loginUrl);
     } else {
       addLinktoLogoutButton(userAuthenticationStatus.logoutUrl);
-      setUpCreatePlaceGuideForm(placeGuideToEdit);
+      setUpCreatePlaceGuideForm();
     }
   });
 }
