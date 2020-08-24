967547090962756edd659d190de5b41b50967b5c
bori00 <fazakasbori@gmail.com>
2020-08-24 20:17:01 +0000

    implement ListPlaceGuideDisplayer partially

--- src/main/webapp/placeGuides/ListPlaceGuideDisplayer.js
+++ src/main/webapp/placeGuides/ListPlaceGuideDisplayer.js
@@ -4,11 +4,46 @@ class ListPlaceGuideDisplayer {
     }
 
     update(placeGuides) {
+        this.removePreviousPlaceGuidesFromList(placeGuides);
+        this.addNewPlaceGuidesToList(placeGuides);
+    }
+
+    removeFromList(placeGuideId) {
 
     }
 
-    remove(placeGuideId) {
+    addToList(placeGuideId) {
 
+    }
+
+    removePreviousPlaceGuidesFromList(placeGuides) {
+        for (var placeGuideId in this._placeGuidesOnList) {
+            if (this._placeGuidesOnList.hasOwnProperty(placeGuideId)) {           
+                if(!placeGuides.hasOwnProperty(placeGuideId)) {
+                    // This placeGuide is not needed anymore.
+                    this.remove(placeGuideId);
+                }
+            }
+        }
+    }
+
+    addNewPlaceGuidesToList(placeGuides) {
+        for (var placeGuideId in placeGuides) {
+            if (placeGuides.hasOwnProperty(placeGuideId)) {           
+                if(!this._placeGuidesOnList.hasOwnProperty(placeGuideId)) {
+                    // new placeGuide should be constructed.
+                    this._placeGuidesOnList[placeGuideId] = this.constructPlaceGuideOnListFromPlaceGuide(placeGuides[placeGuideId]);
+                    this.addToList(placeGuideId);
+                }
+            }
+        }
+    }
+
+    remove(placeGuideId) {
+        // Delete from list.
+        this.removeFromList(placeGuideIde);
+        // Delete from memory.
+        delete this._placeGuidesOnList[placeGuideId];
     }
 
     highlight(placeGuideId) {
@@ -17,5 +52,18 @@ class ListPlaceGuideDisplayer {
 
     unhighlight(placeGuideId) {
         
+    }
+
+    constructPlaceGuideOnListFromPlaceGuide(placeGuide) {
+        return null ;
+        // new PlaceGuideOnList(placeGuide.id, 
+        //                            placeGuide.location, 
+        //                            placeGuide.name, 
+        //                            placeGuide.creator,
+        //                            placeGuide.description, 
+        //                            placeGuide.audioKey,
+        //                            placeGuide.audioLength,
+        //                            placeGuide.imgKey, 
+        //                            placeGuide.createdByCurrentUser);
     }
 }
