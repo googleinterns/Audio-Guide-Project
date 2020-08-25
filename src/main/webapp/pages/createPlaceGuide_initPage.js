ced90f9b157b1a3c9d55f0c6e6b76a0cd208e007
bori00 <fazakasbori@gmail.com>
2020-08-25 19:57:10 +0000

    reorganise pages initialisation

---
+++ src/main/webapp/pages/createPlaceGuide_initPage.js
@@ -0,0 +1,45 @@
+/**
+ * This function initialises the map and adds some functionalities to it:
+ * Geolocation: to track the user's current location, display it and center the map around it.
+ * Choose location: the user can choose  alocation through the searchbox,
+ * by clicking on any point on the map, by clicking on a POI
+ * or by moving the marker of the chosen location.
+ * Save location: lets the user save the currently chosen location for the place guide
+ * creation.
+ * Display place guides: displays the place guides created by the currently logged in user.
+ */
+
+var map;
+var placeGuideManager;
+
+function initPage() {
+    authenticateUser().then(userAuthenticationStatus => {
+        if (!userAuthenticationStatus.isLoggedIn) {
+            location.replace(userAuthenticationStatus.loginUrl);
+        } else {
+            addLinktoLogoutButton(userAuthenticationStatus.logoutUrl);
+            setUpCreatePlaceGuideForm();
+            var mapWidget = new MapWidget();
+            mapWidget.addGeolocationFunctionality();
+            mapWidget.addLocationChoosingAndSavingFunctionality();
+            map = mapWidget.map;
+            var placeGuideRepository = new PlaceGuideRepository(PlaceGuideRepository.QueryType.CREATED_ALL_IN_MAP_AREA);
+            placeGuideManager = new PlaceGuideManager(placeGuideRepository);
+            google.maps.event.addListener(map, 'idle', function() {
+                    placeGuideManager.update(map.getBounds(), map.getZoom());
+            });
+            document.getElementById("map")
+                    .addEventListener(MapWidget.SAVE_LOCATION_EVENT, function () {
+                        handleSaveLocationEvent(mapWidget);
+                    });
+        }
+  });
+}
+
+function handleSaveLocationEvent(mapWidget) {
+     if (mapWidget.savedLocation.place != null) {
+        updateLocation(mapWidget.savedLocation.position, mapWidget.savedLocation.place.place_id, mapWidget.savedLocation.place.name);
+    } else {
+        updateLocation(mapWidget.savedLocation.position, null, null);
+    }
+}
---
+++ src/main/webapp/pages/discover_initPage.js
@@ -0,0 +1,26 @@
+/**
+ * This function initialises the map and adds some functionalities to it:
+ * Geolocation: to track the user's current location, display it and center the map around it.
+ * Searching: lets the user search for places and center the map around it.
+ * Display place guides: displays all the public place guides.
+ */
+var map;
+var placeGuideManager;
+
+function initPage() {
+    authenticateUser().then(userAuthenticationStatus => {
+        if (!userAuthenticationStatus.isLoggedIn) {
+            location.replace(userAuthenticationStatus.loginUrl);
+        } else {
+            var mapWidget = new MapWidget();
+            mapWidget.addGeolocationFunctionality();
+            mapWidget.addSearchingFunctionality();
+            map = mapWidget.map;
+            var placeGuideRepository = new PlaceGuideRepository(PlaceGuideRepository.QueryType.ALL_PUBLIC_IN_MAP_AREA);
+            placeGuideManager = new PlaceGuideManager(placeGuideRepository);
+            google.maps.event.addListener(map, 'idle', function() {
+                    placeGuideManager.update(map.getBounds(), map.getZoom());
+            });
+        }
+    });
+}
