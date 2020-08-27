b8beb3570d42962183b71b4ad4c7aff14927d838
bori00 <fazakasbori@gmail.com>
2020-08-27 11:28:53 +0000

    add links to menu

--- src/main/webapp/menu.js
+++ src/main/webapp/menu.js
@@ -2,6 +2,27 @@ const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 const tabs = document.querySelectorAll('.mdc-tab');
 
 tabBar.listen('MDCTabBar:activated', function(event) {
-  //let tab = tabs[event.detail.index];
-  console.log(event.detail.index, 'tab activated');
+  var url;
+  switch(event.detail.index) {
+    case 0:
+        // The portfolio page is currently on index.html.
+        url = new URL("/index.html", document.URL);
+        break;
+    case 1:
+        var url = new URL("/discover.html", document.URL);
+        break;
+    case 2:
+        var url = new URL("/myPlaceGuides.html", document.URL);
+        break;
+    case 3:
+        var url = new URL("createPlaceGuide.html", document.URL);
+        break;
+    case 4:
+        // The bookmarkedPlaceGuides page is not ready yet.
+        var url = new URL("/index.html", document.URL);
+        break;
+    default:
+        var url = new URL(document.url);
+  }
+  window.location = url;
 });
