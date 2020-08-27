949085fd4eeeb81f6587c6ef3fb07c369a5aefca
bori00 <fazakasbori@gmail.com>
2020-08-27 14:16:05 +0000

    generate tabs dynamically

--- src/main/webapp/menu.js
+++ src/main/webapp/menu.js
@@ -41,12 +41,36 @@ class Menu {
     }
 
     constructor(pageName) {
+        const NO_TABS = 5;
         const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
-        const tabs = document.querySelectorAll('.mdc-tab');
+        var tabs = [];
+        for (var i = 0; i < NO_TABS; i++) {
+            var tab = this.createMenuTab(i);
+            document.querySelector('.mdc-tab-scroller__scroll-content').appendChild(tab);
+            tabs.push(tab);
+        }
         tabBar.activateTab(pageName.index);
         tabBar.listen('MDCTabBar:activated', function(event) {
+            console.log("activated tab: " + event.detail.index);
             var url = new URL(Menu.PAGE_NUMBERS[event.detail.index].url, document.URL);
             window.location = url;
         });
+    }
+
+    createMenuTab(index) {
+        var tab = document.createElement("button");
+        tab.setAttribute("class", "mdc-tab mdc-tab--active");
+        tab.setAttribute("role", "tab");
+        tab.setAttribute("aria-selected", "true");
+        tab.setAttribute("tabindex", "0");
+        tab.innerHTML = "<span class=\"mdc-tab__content\">\
+                                <span class=\"mdc-tab__icon material-icons\" aria-hidden=\"true\">" + Menu.PAGE_NUMBERS[index].icon + "</span>\
+                                <span class=\"mdc-tab__text-label\">" + Menu.PAGE_NUMBERS[index].label + "</span>\
+                            </span>\
+                            <span class=\"mdc-tab-indicator\">\
+                                <span class=\"mdc-tab-indicator__content mdc-tab-indicator__content--underline\"></span>\
+                            </span>\
+                            <span class=\"mdc-tab__ripple\"></span>";
+        return tab;
     }
 }
