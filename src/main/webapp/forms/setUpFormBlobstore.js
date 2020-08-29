6a856b9c27e1f01d6cbba9dc15b78dfc22d49767
Dennis Willie <denniswillie2000@gmail.com>
2020-08-29 13:23:42 +0000

    add setSrcToElementOnChangeEvent to setupFormBlobstore.js

--- src/main/webapp/forms/setUpFormBlobstore.js
+++ src/main/webapp/forms/setUpFormBlobstore.js
@@ -46,3 +46,25 @@ function setFormInputValue(input, value) {
     input.value = value;
   }
 }
+
+function setSrcToElementOnChangeEvent(elementId, previewId, displayBlock) {
+  const element = document.getElementById(elementId);
+  const preview = document.getElementById(previewId);
+  element.addEventListener("change", function() {
+    const file = this.files[0];
+    console.log(this.files);
+
+    if (file) {
+      const reader = new FileReader();
+      if (displayBlock) {
+        preview.style.display = "block"; 
+      }
+
+      reader.addEventListener("load", function() {
+        preview.setAttribute("src", this.result);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+}
