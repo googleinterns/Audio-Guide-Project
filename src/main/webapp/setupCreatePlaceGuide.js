8e59260d8ba456d3de407acf96849f3fa7881bfd
Dennis Willie <denniswillie2000@gmail.com>
2020-08-29 10:31:45 +0000

    delete setApiKeyAndMapsCallback function in setupCreatePlaceGuide.js for testing purposes

--- src/main/webapp/setupCreatePlaceGuide.js
+++ src/main/webapp/setupCreatePlaceGuide.js
@@ -3,7 +3,6 @@ function setUpCreatePlaceGuidePage() {
     if (!userAuthenticationStatus.isLoggedIn) {
       location.replace(userAuthenticationStatus.loginUrl);
     } else {
-      setApiKeyAndMapsCallback();
       addLinktoLogoutButton(userAuthenticationStatus.logoutUrl);
       setUpCreatePlaceGuideForm();
     }
