1e749bc3d3cc47d6a0708f35ebdd808371a04636
bori00 <fazakasbori@gmail.com>
2020-09-02 23:40:40 +0000

    implement UserRepresentation

--- src/main/webapp/user/UserRepresentation.js
+++ src/main/webapp/user/UserRepresentation.js
@@ -1,25 +1,49 @@
 class UserRepresentation {
   constructor(user) {
-    this._userRepresentationDiv = createUserDiv(user);
+    this._userRepresentationDiv = this.createUserDiv(user);
   }
 
   get userRepresentationDiv() {
     return this._userRepresentationDiv;
   }
-}
 
-function createUserDiv(user) {
-  const userDiv = document.createElement("div");
-  const userDivClass = "userRepresentation-" + "{" + user.id() + "}";
-  userDiv.setAttribute("class", userDivClass);
-//   userDiv.addEventListener("click", function() {
-//     if (user.publicPortfolio) {
-//       var url = new URL("/portfolio.html", document.URL);
-//       url.searchParams.append('user', user)
-//       window.location = url;
-//     } else {
-//       alert("The creator of this place guide does not have a private place guide!");
-//     }
-//   });
-  return userDiv;
+  createUserDiv(user) {
+    const userDiv = document.createElement("div");
+    const userDivClass = "userRepresentation-" + "{" + user.id() + "}";
+    userDiv.setAttribute("class", userDivClass);
+    if (user.imgKey != undefined) {
+        userDiv.appendChild(this.createUserImg(user.imgKey));
+    }
+    if (user.name != undefined) {
+        userDiv.appendChild(this.createUserNameText(user.name));
+    } else {
+        userDiv.appendChild(this.createUserNameText(user.email));
+    }
+    //   userDiv.addEventListener("click", function() {
+    //     if (user.publicPortfolio) {
+    //       var url = new URL("/portfolio.html", document.URL);
+    //       url.searchParams.append('user', user)
+    //       window.location = url;
+    //     } else {
+    //       alert("The creator of this place guide does not have a private place guide!");
+    //     }
+    //   });
+    return userDiv;
+  }
+
+  createUserImg(imgKey) {
+    const userImg = document.createElement("img");
+    userDiv.setAttribute("class", "user-img");
+    const src = new URL("/serve-blob", document.URL);
+    src.searchParams.append('blob-key', imgKey);
+    userImg.setAttribute("src", src);
+    return userImg;
+  }
+
+  createUserNameText(name) {
+    const userText = document.createElement("p");
+    // userDiv.setAttribute("class", "user-img");
+    userText.innerHTML = name;
+    return userText;
+  }
 }
