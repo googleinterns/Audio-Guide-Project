2f6e91884bf85bec1743ca7790fa971ce8b563bf
bori00 <fazakasbori@gmail.com>
2020-09-11 16:39:26 +0000

    init UserRepresentation

---
+++ src/main/webapp/user/UserRepresentation.js
@@ -0,0 +1,65 @@
+class UserRepresentation {
+  constructor(user) {
+    this._userRepresentationDiv = this.createUserDiv(user);
+  }
+
+  get userRepresentationDiv() {
+    return this._userRepresentationDiv;
+  }
+
+  static createUserDiv(user) {
+    const userDiv = document.createElement("div");
+    userDiv.style.height = "5px";
+    userDiv.style.width = "50px";
+    const userDivClass = "userRepresentation-" + "{" + user.id + "}";
+    userDiv.setAttribute("class", userDivClass);
+    if (user.imgKey != undefined) {
+      if (user.name != undefined) {
+        userDiv.appendChild(
+            UserRepresentation.createUserImg(user.imgKey, user.name));
+      } else {
+        userDiv.appendChild(
+            UserRepresentation.createUserImg(user.imgKey, user.email));
+      }
+    } else {
+      if (user.name != undefined) {
+        userDiv.appendChild(
+            UserRepresentation.createUserIcon(user.name));
+      } else {
+        userDiv.appendChild(
+            UserRepresentation.createUserIcon(user.email));
+      }
+    }
+    //   userDiv.addEventListener("click", function() {
+    //     if (user.publicPortfolio) {
+    //       var url = new URL("/portfolio.html", document.URL);
+    //       url.searchParams.append('user', user)
+    //       window.location = url;
+    //     } else {
+    //       alert("The creator of this place guide does not have a private place guide!");
+    //     }
+    //   });
+    return userDiv;
+  }
+
+  static createUserImg(imgKey, name) {
+    const userImg = document.createElement("img");
+    userImg.setAttribute("class", "user-img");
+    const src = new URL("/serve-blob", document.URL);
+    src.searchParams.append('blob-key', imgKey);
+    userImg.setAttribute("src", src);
+    userImg.setAttribute("title", "Owner: " + name);
+    return userImg;
+  }
+
+  static createUserIcon(name) {
+    const userIcon = document.createElement("i");
+    userIcon.classList.add("material-icons",
+        "mdc-icon-button",
+        "mdc-card__action",
+        "mdc-card__action--icon");
+    userIcon.innerText = "account_circle";
+    userIcon.setAttribute("title", "Owner: " + name);
+    return userIcon;
+  }
+}
