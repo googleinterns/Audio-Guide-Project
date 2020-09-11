1883b560fbd06fb7e881644dc22a3d5bcdc2ae18
bori00 <fazakasbori@gmail.com>
2020-09-11 16:35:45 +0000

    generate and decode querystring for user

--- src/main/webapp/user/User.js
+++ src/main/webapp/user/User.js
@@ -31,4 +31,39 @@ class User {
   get imgKey() {
     return this._imgKey;
   }
+
+  get queryString() {
+    const query = `id=${this._id}&name=${this._name}&email=${this._email}&publicPortfolio=${this._publicPortfolio}&selfIntroduction=${this._selfIntroduction}&imgKey=${this._imgKey}`;
+    return query;
+  }
+
+  static getUserFromQueryString() {
+    if (window.location.search != '') {
+      const GET = {};
+      const queryString = decodeURI(window.location.search.replace(/^\?/, ''));
+      queryString.split(/\&/).forEach(function(keyValuePair) {
+        const paramName = keyValuePair.replace(/=.*$/, '');
+        const paramValue = keyValuePair.replace(/^[^=]*\=/, '');
+        GET[paramName] = paramValue;
+      });
+      const id = GET['id'];
+      let name = GET['name'];
+      if (name == "undefined") {
+        name = undefined;
+      }
+      const email = GET['email'];
+      const selfIntroduction = GET['selfIntroduction'];
+      let imgKey = GET['imgKey'];
+      if (imgKey == "undefined") {
+        imgKey = undefined;
+      }
+      let publicPortfolio = GET['publicPortfolio'];
+      if (publicPortfolio == "true") {
+        publicPortfolio = true;
+      } else {
+        publicPortfolio = false;
+      }
+      return new User(id, email, name, publicPortfolio, selfIntroduction, imgKey);
+    }
+  }
 }
