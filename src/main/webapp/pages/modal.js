bf3bb80c496767e673adbb779bff83531462c89d
bori00 <fazakasbori@gmail.com>
2020-09-23 20:14:31 +0000

    restructure code

--- src/main/webapp/pages/modal.js
+++ src/main/webapp/pages/modal.js
@@ -2,20 +2,33 @@ class Modal {
   static show(text, timeLimit) {
     const modal = document.getElementById("myModal");
     modal.style.display = "block";
+    Modal.setModalText()
+    Modal.closeModalOnButtonClick();
+    Modal.closeModalOnWindowClick();
+    if (timeLimit !== undefined) {
+      setTimeout(Modal.hide, timeLimit);
+    }
+  }
+
+  static setModalText(text) {
     const modalText = document.getElementById("modalText");
     modalText.innerText = text;
-    var btn = document.getElementById("closeModalBtn");
+  }
+
+  static closeModalOnButtonClick() {
+    const btn = document.getElementById("closeModalBtn");
     btn.addEventListener("click", function() {
       Modal.hide();
     });
+  }
+
+  static closeModalOnWindowClick() {
+    const modal = document.getElementById("myModal");
     window.addEventListener("click", function(event) {
       if (event.target === modal) {
         Modal.hide();
       }
     });
-    if (timeLimit !== undefined) {
-      setTimeout(Modal.hide, timeLimit);
-    }
   }
 
   static hide() {
